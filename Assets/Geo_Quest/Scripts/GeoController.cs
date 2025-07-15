c7fd931fb97ba2e9b097dd5a315aa048d9af57ec
joseynf26 <josealejandro4007@gmail.com>
2025-07-15 15:28:08 -0400

    15/7

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -29,7 +29,8 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
+   
+            float xInput = Input.GetAxis("Horizontal");
         player.velocity = new Vector2(xInput*speed, player.velocity.y); 
 
         Debug.Log(varTwo);
